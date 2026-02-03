e5e3a46ca0788cf5417467902cd63fb6ac02eb4a
kirankhobragade1829-source <kirankhobragade1829@gmail.com>
2026-02-03 12:55:20 +0530

    Update kiran_khobragade.sh

--- scripts/kiran_khobragade.sh
+++ scripts/kiran_khobragade.sh
@@ -1,2 +1,3 @@
 #Downloading the genome assigned to me
 #Sus_scrofa
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/003/025/GCA_000003025.7_T2T-Sscrofa/GCA_000003025.7_T2T-Sscrofa_genomic.fna.gz
