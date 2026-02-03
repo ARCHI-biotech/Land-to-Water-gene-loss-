b0d253e375e3f09052a7d8fb416bafd1b054ce2b
patelsurya132-spec <patelsurya132@gmail.com>
2026-02-03 14:19:55 +0530

    Update surya_patel.sh

--- scripts/surya_patel.sh
+++ scripts/surya_patel.sh
@@ -3,7 +3,6 @@
 efetch -db nucleotide -id NM_000505.4 -format fasta_cds_na > F12_human_CDS.fasta
 Downloading the genome -
 # Erinaceus europaeus
-# Download hedgehog genome
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/950/295/315/GCF_950295315.1_mEriEur2.1/GCF_950295315.1_mEriEur2.1_genomic.fna.gz
 # Unzip genome
 gunzip GCF_950295315.1_mEriEur2.1_genomic.fna.gz
