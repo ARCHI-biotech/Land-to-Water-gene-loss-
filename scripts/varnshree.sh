3b5dc728b26a171f9fe0a12b195707441ba20737
varnshree273-vs <varnshree273@gmail.com>
2026-02-03 15:27:34 +0530

    Update varnshree.sh

--- scripts/varnshree.sh
+++ scripts/varnshree.sh
@@ -1,10 +1,20 @@
 #Retrieve human query sequences from annotation
 #gene F12
 esearch -db nuccore -query "NM_000505.5" | efetch -format fasta_cds_na > F12_CDS.fasta
+
 #Downloading the genome assigned to me
 #Trichechus_manatus
-wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/030/014/295/GCF_030014295.1_mLoxAfr1.hap2/GCF_030014295.1_mLoxAfr1.hap2_cds_from_genomic.fna.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/243/295/GCF_000243295.1_TriManLat1.0/GCF_000243295.1_TriManLat1.0_genomic.fna.gz
 #Command to unzip the file
 gunzip GCF_000243295.1_TriManLat1.0_genomic.fna.gz
 #Create BLAST Database
 makeblastdb -in GCF_000243295.1_TriManLat1.0_genomic.fna -dbtype nucl -out manatee_db
+
+
+#Downloading the genome assigned to me
+#Loxodonta_africana
+ wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/030/014/295/GCF_030014295.1_mLoxAfr1.hap2/GCF_030014295.1_mLoxAfr1.hap2_cds_from_genomic.fna.gz
+#Command to unzip the file
+gunzip GCF_030014295.1_mLoxAfr1.hap2_cds_from_genomic.fna.gz
+#Create BLAST Database
+makeblastdb -in GCF_030014295.1_mLoxAfr1.hap2_cds_from_genomic.fna -dbtype nucl -out elephant_cds_db
