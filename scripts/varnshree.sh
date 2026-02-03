9724f767985c14fff51e9e9b4c8e3ee4e8b6cf15
varnshree273-vs <varnshree273@gmail.com>
2026-02-03 14:20:10 +0530

    Update varnshree.sh

--- scripts/varnshree.sh
+++ scripts/varnshree.sh
@@ -1,1 +1,10 @@
-
+#Retrieve human query sequences from annotation
+#gene F12
+esearch -db nuccore -query "NM_000505.5" | efetch -format fasta_cds_na > F12_CDS.fasta
+#Downloading the genome assigned to me
+#Trichechus manatus
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/030/014/295/GCF_030014295.1_mLoxAfr1.hap2/GCF_030014295.1_mLoxAfr1.hap2_cds_from_genomic.fna.gz
+#Command to unzip the file
+gunzip GCF_000243295.1_TriManLat1.0_genomic.fna.gz
+#Create BLAST Database
+makeblastdb -in GCF_000243295.1_TriManLat1.0_genomic.fna -dbtype nucl -out manatee_db
