bec44bdf5e85d87472d8851f8626ebd465a97e33
kirankhobragade1829-source <kirankhobragade1829@gmail.com>
2026-02-03 13:56:46 +0530

    Update Shruti_kardile.sh

--- scripts/Shruti_kardile.sh
+++ scripts/Shruti_kardile.sh
@@ -4,3 +4,11 @@ efetch -db nucleotide -id NM_000505.4 -format fasta_cds_na > F12_human_CDS.fasta
 #Downloading the genome assigned to me
 #Giraffa_camelopardalis
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/017/591/445/GCA_017591445.1_ASM1759144v1/GCA_017591445.1_ASM1759144v1_genomic.fna.gz
+#Command to unzip the file
+gunzip GCA_017591445.1_ASM1759144v1_genomic.fna.gz
+#Create BLAST Database
+makeblastdb \
+  -in GCA_017591445.1_ASM1759144v1_genomic.fna \
+  -dbtype nucl \
+  -parse_seqids \
+  -out Griaffa_camelopardalis_genome_db
