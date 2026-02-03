17d2139d2dc091d30e5f5ac45601190558a9a79f
varnshree273-vs <varnshree273@gmail.com>
2026-02-03 15:18:15 +0530

    Update varnshree.sh

--- scripts/varnshree.sh
+++ scripts/varnshree.sh
@@ -2,7 +2,7 @@
 #gene F12
 esearch -db nuccore -query "NM_000505.5" | efetch -format fasta_cds_na > F12_CDS.fasta
 #Downloading the genome assigned to me
-#Trichechus manatus
+#Trichechus_manatus
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/030/014/295/GCF_030014295.1_mLoxAfr1.hap2/GCF_030014295.1_mLoxAfr1.hap2_cds_from_genomic.fna.gz
 #Command to unzip the file
 gunzip GCF_000243295.1_TriManLat1.0_genomic.fna.gz
