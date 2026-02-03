6f16aa600fa77ff305e9025080a595ca62d866f5
ceglab <52042219+ceglab@users.noreply.github.com>
2026-02-03 12:13:01 +0530

    genomes

--- scripts/Archi.sh
+++ scripts/Archi.sh
@@ -1,1 +1,3 @@
-https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/023/091/745/GCF_023091745.1_Takin1.1/GCF_023091745.1_Takin1.1_genomic.fna.gz
+#Downloading the genomes assigned to me
+#Budorcas_taxicolor
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/023/091/745/GCF_023091745.1_Takin1.1/GCF_023091745.1_Takin1.1_genomic.fna.gz
