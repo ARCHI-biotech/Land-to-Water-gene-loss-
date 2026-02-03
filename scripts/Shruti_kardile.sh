bf87dc3482d29e6a0e6a1ec4ef1314b9d9b7f508
kirankhobragade1829-source <kirankhobragade1829@gmail.com>
2026-02-03 13:19:02 +0530

    Update Shruti_kardile.sh

--- scripts/Shruti_kardile.sh
+++ scripts/Shruti_kardile.sh
@@ -1,1 +1,3 @@
-
+#Downloading the genome assigned to me
+#Giraffa_camelopardalis
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/017/591/445/GCA_017591445.1_ASM1759144v1/GCA_017591445.1_ASM1759144v1_genomic.fna.gz
