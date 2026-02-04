8d2ddf8e73dc56ccdab11afabfe429a9fbc7ffb2
varnshree273-vs <varnshree273@gmail.com>
2026-02-04 14:12:09 +0530

    Update varnshree.sh

--- scripts/varnshree.sh
+++ scripts/varnshree.sh
@@ -1,9 +1,9 @@
 #Bubalus_bubalis
 weget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/019/923/935/GCF_019923935.1_NDDB_SH_1/GCF_019923935.1_NDDB_SH_1_genomic.fna.gz
 #Command to unzip the file
-
+gunzip GCF_019923935.1_NDDB_SH_1_genomic.fna.gz
 #Create BLAST Database
-
+makeblastdb -in GCF_019923935.1_NDDB_SH_1_genomic.fna -dbtype nucl -out bubalus_bubalis_db
 
   #Bison_bison
   wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/754/665/GCF_000754665.1_Bison_UMD1.0/GCF_000754665.1_Bison_UMD1.0_genomic.fna.gz
