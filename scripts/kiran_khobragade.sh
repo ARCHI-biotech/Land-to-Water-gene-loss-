bdd17f420fdd09a5dca6782502d2686128a6bcb7
kirankhobragade1829-source <kirankhobragade1829@gmail.com>
2026-02-04 13:09:04 +0530

    Update kiran_khobragade.sh

--- scripts/kiran_khobragade.sh
+++ scripts/kiran_khobragade.sh
@@ -12,3 +12,18 @@ makeblastdb \
   -dbtype nucl \
   -parse_seqids \
   -out sus_scrofa_genome_db
+#Downloaded the genome assigned to me
+#Hippopotamus_amphibius
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/030/028/045/GCF_030028045.1_mHipAmp2.hap2/GCF_030028045.1_mHipAmp2.hap2_genomic.fna.gz
+#Command for unzip the file
+gunzip GCF_030028045.1_mHipAmp2.hap2_genomic.fna.gz
+#Create BLAST Database
+ makeblastdb \
+-in GCF_030028045.1_mHipAmp2.hap2_genomic.fna \
+-dbtype nucl \
+-parse_seqids \
+-out Hippo_hap2_genome_db
+#downloaded the genome assigned to me
+#Eubalaena_japonica
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/004/363/455/GCA_004363455.1_EubJap_v1_BIUU/GCA_004363455.1_EubJap_v1_BIUU_genomic.fna.gz
+#Command to unzip the file
