ffadfd2459bbab83f3a4830999623147bbad03bb
varnshree273-vs <varnshree273@gmail.com>
2026-02-04 13:33:04 +0530

    Update varnshree.sh

--- scripts/varnshree.sh
+++ scripts/varnshree.sh
@@ -1,3 +1,4 @@
+
 #Retrieve human query sequences from annotation
 #gene F12
 esearch -db nuccore -query "NM_000505.5" | efetch -format fasta_cds_na > F12_CDS.fasta
@@ -18,3 +19,13 @@ makeblastdb -in GCF_000243295.1_TriManLat1.0_genomic.fna -dbtype nucl -out manat
 gunzip GCF_030014295.1_mLoxAfr1.hap2_cds_from_genomic.fna.gz
 #Create BLAST Database
 makeblastdb -in GCF_030014295.1_mLoxAfr1.hap2_cds_from_genomic.fna -dbtype nucl -out elephant_cds_db
+
+Physeter_macrocephalus
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/472/395/GCF_000472395.1_PhysMacro1.0/GCF_000472395.1_PhysMacro1.0_genomic.fna.gz
+#Command to unzip the file
+gunzip GCF_002837175.3_ASM283717v5_genomic.fna.gz
+makeblastdb \
+  -in GCF_000472395.1_PhysMacro1.0_genomic.fna \
+  -dbtype nucl \
+  -parse_seqids \
+  -out Physeter_macrocephalus_genome_db
