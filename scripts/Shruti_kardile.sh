b22fcf687c81872f50a9083b0402878e16d21345
kirankhobragade1829-source <kirankhobragade1829@gmail.com>
2026-02-04 13:28:30 +0530

    Update Shruti_kardile.sh

--- scripts/Shruti_kardile.sh
+++ scripts/Shruti_kardile.sh
@@ -2,13 +2,24 @@
 #gene F12
 efetch -db nucleotide -id NM_000505.4 -format fasta_cds_na > F12_human_CDS.fasta
 #Downloading the genome assigned to me
-#Giraffa_camelopardalis
-wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/017/591/445/GCA_017591445.1_ASM1759144v1/GCA_017591445.1_ASM1759144v1_genomic.fna.gz
+#Megaptera_novaeangliae
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/041/834/305/GCA_041834305.1_ASM4183430v1/GCA_041834305.1_ASM4183430v1_genomic.fna.gz
 #Command to unzip the file
-gunzip GCA_017591445.1_ASM1759144v1_genomic.fna.gz
+gunzip GCA_041834305.1_ASM4183430v1_genomic.fna.gz
 #Create BLAST Database
 makeblastdb \
-  -in GCA_017591445.1_ASM1759144v1_genomic.fna \
+  -in GCA_041834305.1_ASM4183430v1_genomic.fna \
   -dbtype nucl \
   -parse_seqids \
-  -out Griaffa_camelopardalis_genome_db
+  -out Megaptera_novaeangliae_genome_db
+#Downloaded the genome assigned to me
+#Eschrichtius_robustus
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/028/021/215/GCF_028021215.1_mEscRob2.pri/GCF_028021215.1_mEscRob2.pri_genomic.gbff.gz
+#Command to unzip the file
+gunzip GCF_028021215.1_mEscRob2.pri_genomic.fna.gz
+#Create BLAST Database
+makeblastdb \
+  -in GCF_028021215.1_mEscRob2.pri_genomic.fna \
+  -dbtype nucl \
+  -parse_seqids \
+  -out Eschrichtius_robustus_genome_db
