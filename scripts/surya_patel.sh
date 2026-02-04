d8ca5ceb615566069dfa8968171d2722c3e8c9e6
patelsurya132-spec <patelsurya132@gmail.com>
2026-02-04 13:45:40 +0530

    Update surya_patel.sh

--- scripts/surya_patel.sh
+++ scripts/surya_patel.sh
@@ -1,3 +1,6 @@
+#Retrieve human query sequences from annotation
+#gene F12
+efetch -db nucleotide -id NM_000505.4 -format fasta_cds_na > F12_human_CDS.fasta
 # Balaenoptera_acutorostrata [minke_whale]
 #Downloading the genome
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/949/987/535/GCF_949987535.1_mBalAcu1.1/GCF_949987535.1_mBalAcu1.1_genomic.fna.gz
