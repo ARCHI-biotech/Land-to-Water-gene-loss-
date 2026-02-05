f4695c6c177adfdf59fedfb2ee752abeefafe5fa
varnshree273-vs <varnshree273@gmail.com>
2026-02-05 12:04:59 +0530

    Update varnshree.sh

--- scripts/varnshree.sh
+++ scripts/varnshree.sh
@@ -1,3 +1,7 @@
+#Retrieve human query sequences from annotation
+#gene F12
+efetch -db nucleotide -id NM_000505.4 -format fasta_cds_na > F12_human_CDS.fasta
+
 #Bubalus_bubalis
 weget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/019/923/935/GCF_019923935.1_NDDB_SH_1/GCF_019923935.1_NDDB_SH_1_genomic.fna.gz
 #Command to unzip the file
@@ -16,10 +20,10 @@ blastn -task dc-megablast -query query.fasta -db bubalus_bubalis_db -outfmt 6 -e
 
 #Bison_bison
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/754/665/GCF_000754665.1_Bison_UMD1.0/GCF_000754665.1_Bison_UMD1.0_genomic.fna.gz
- #Command to unzip the file
- gunzip GCF_000754665.1_Bison_UMD1.0_genomic.fna.gz
- #Create BLAST Database
-  makeblastdb -in GCF_000754665.1_Bison_UMD1.0_genomic.fna -dbtype nucl -out bison_db
+#Command to unzip the file
+gunzip GCF_000754665.1_Bison_UMD1.0_genomic.fna.gz
+#Create BLAST Database
+makeblastdb -in GCF_000754665.1_Bison_UMD1.0_genomic.fna -dbtype nucl -out bison_db
 #short blast-outfmt3 evalue0.001
 blastn -task blastn-short -query query.fasta -db bison_bison_db -outfmt 3 -evalue 0.001 -dust no -num_threads 8 -max_target_seqs 10 -out short_fmt3.blast
 #short blast-outfmt6 evalue0.001
@@ -29,17 +33,17 @@ blastn -task dc-megablast -query query.fasta -db bison_bison_db -outfmt 3 -evalu
 #dc-megablast outfmt6 evalue0.001
 blastn -task dc-megablast -query query.fasta -db bison_bison_db -outfmt 6 -evalue 0.001 -dust no -num_threads 8 -out dcmegablast_fmt6.txt
 
- #Physeter_macrocephalus
+#Physeter_macrocephalus
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/472/395/GCF_000472395.1_PhysMacro1.0/GCF_000472395.1_PhysMacro1.0_genomic.fna.gz
 #Command to unzip the file
 gunzip GCF_002837175.3_ASM283717v5_genomic.fna.gz
 #Create BLAST Database
 makeblastdb \
-  -in GCF_000472395.1_PhysMacro1.0_genomic.fna \
-  -dbtype nucl \
-  -parse_seqids \
-  -out Physeter_macrocephalus_genome_db
-  #short blast-outfmt3 evalue0.001
+-in GCF_000472395.1_PhysMacro1.0_genomic.fna \
+-dbtype nucl \
+-parse_seqids \
+-out Physeter_macrocephalus_genome_db
+#short blast-outfmt3 evalue0.001
 blastn -task blastn-short -query query.fasta -db Physeter_macrocephalus_genome_db -outfmt 3 -evalue 0.001 -dust no -num_threads 8 -max_target_seqs 10 -out short_fmt3.blast
  #short blast-outfmt6 evalue0.001
 blastn -task blastn-short -query query.fasta -db Physeter_macrocephalus_genome_db -outfmt 6 -evalue 0.001 -dust no -num_threads 8 -max_target_seqs 10 -out short_fmt6.txt
